ddc9a4b932a582a1d37fba6aaf678aca926a7a30
Eddy Jo <eddyjo@Eddys-MacBook-Pro.local>
2023-03-22 05:50:26 -0400

    feat: add tailwind aspect-ratio to optimize video view

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -20,14 +20,14 @@ export default function VideoDetail() {
     <Wrapper>
       <div className="flex flex-col lg:flex-row mt-3">
         <div className="basis-4/6">
-          <iframe
-            id="player"
-            type="text/html"
-            width="100%"
-            height="640"
-            src={videoURL}
-            title={title}
-          ></iframe>
+          <div className="aspect-w-16 aspect-h-9">
+            <iframe
+              id="player"
+              type="text/html"
+              src={videoURL}
+              title={title}
+            ></iframe>
+          </div>
 
           <div className="p-8">
             <p className="text-xl font-bold">{title}</p>
